485c5f7e35b1379ca541cfd559a24971e6270c04
Anuj711 <54545961+Anuj711@users.noreply.github.com>
2020-05-25 22:22:24 -0400

    Added "Date posted"

--- redditScraping.py
+++ redditScraping.py
@@ -10,7 +10,7 @@ def main():
                          password="$15@Anuj",
                          user_agent="Scraping",
                          username="warrior21q")
-    free_Games = reddit.subreddit("FreeGameFindings").new(limit=17)
+    free_Games = reddit.subreddit("FreeGameFindings").new(limit=7)
 
     # Message we are trying to send
     message = ""
@@ -24,7 +24,7 @@ def main():
         print(title)
         date = int(post.created_utc)
         convertedDate = str(datetime.datetime.utcfromtimestamp(date))
-        if "GOG" in post.title or "Epic" in post.title or "Humble" in post.title or "Uplay" in post.title or "UPLAY" in post.title or "uplay" in post.title or "PS4" in post.title:
+        if "GOG" in post.title or "Epic" in post.title or "Humble" in post.title or "Uplay" in post.title or "UPLAY" in post.title or "uplay" in post.title or "PS4" in post.title or "steam" in post.title or "Steam" in post.title:
             mainGamesCounter += 1
             mainGames2.append(title)
             mainGamesDate.append(convertedDate)
@@ -37,11 +37,7 @@ def main():
     else:
         message += ("\nGames for this week:")
         for game in mainGames2:
-            message += ('\n')
-            message += ('-')
-            message += game
-            message += " : "
-            message += mainGamesDate[dateListCounter]
+            message += '\n' + '-' + game + ", Posted on: " + mainGamesDate[dateListCounter]
             dateListCounter += 1
         message.encode('utf-8')
 
@@ -50,7 +46,7 @@ def main():
     port = 587  # For starttls
     smtp_server = "smtp.gmail.com"
     sender_email = "anujshukla01@gmail.com"
-    receiver_email = "anujshukla01@gmail.com"
+    receiver_email = "anujshukla01@gmail.com", "shukla_animesh@hotmail.com"
     password = "$19@Anuj"
 
     context = ssl.create_default_context()
